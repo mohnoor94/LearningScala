8c3776869d03290bbbd434519acf9d4ac5b4f9a3
abukhleif <moh.noor94@gmail.com>
2018-04-18 22:15:05 +0300

    Update 1 example and another 1 to collection functions

--- src/main/scala/_04_collections_with_functions/GeneralExample04_ScalarProduct.scala
+++ src/main/scala/_04_collections_with_functions/GeneralExample04_ScalarProduct.scala
@@ -12,7 +12,10 @@ object GeneralExample04_ScalarProduct extends App {
   def scalarProduct2(xs: Vector[Double], ys: Vector[Double]): Double =
     (xs zip ys).map { case (x, y) => x * y }.sum
 
+  def scalarProduct3(xs: Vector[Double], ys: Vector[Double]): Double =
+    (for ((x, y) <- xs zip ys) yield x * y).sum
 
   println(scalarProduct1(Vector(2.5, 3.5, 4.5), Vector(5.6, 6, 9)))
   println(scalarProduct2(Vector(2.5, 3.5, 4.5), Vector(5.6, 6, 9)))
+  println(scalarProduct3(Vector(2.5, 3.5, 4.5), Vector(5.6, 6, 9)))
 }
