0f973c8727bcac8e2c8bf16ca0461538fe10060a
Mohammad Noor <moh.noor94@gmail.com>
2018-05-09 15:19:22 +0300

    add a stub for 'Future' package in _09_timely_effects section, to be completed later

---
+++ src/main/scala/_09_timely_effects/_03_Futures/package.scala
@@ -0,0 +1,8 @@
+package _09_timely_effects
+
+//TODO Add this section!
+// I don't like the way this topic taught ed in 'Functional Design Principles' courses
+// so I will postpone adding this until I find a better resource
+package object _03_Futures {
+
+}
