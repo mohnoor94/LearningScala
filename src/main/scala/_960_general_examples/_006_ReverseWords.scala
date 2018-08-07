11c4d5de09c363294fb44eedefeb9eea8568c997
AbuKhleif <moh.noor94@gmail.com>
2018-08-08 01:19:55 +0300

    trim the string before processing it in '_006_ReverseWords' example

--- src/main/scala/_960_general_examples/_006_ReverseWords.scala
+++ src/main/scala/_960_general_examples/_006_ReverseWords.scala
@@ -1,10 +1,10 @@
 package _960_general_examples
 
 object _006_ReverseWords {
-  //  def reverseWords(sentence: String): String = sentence.split("\\s").map(_.reverse).mkString(" ")
-  val reverseWords: String => String = (sentence: String) => sentence.split("\\s").map(_.reverse).mkString(" ")
+  //    def reverseWords(sentence: String): String = sentence.trim.split("\\s+").map(_.reverse).mkString(" ")
+  val reverseWords: String => String = (sentence: String) => sentence.trim.split("\\s+").map(_.reverse).mkString(" ")
 
   def main(args: Array[String]): Unit = {
-    println(reverseWords("Hello my world!"))
+    println(reverseWords("  Hello my    world!    "))
   }
 }
