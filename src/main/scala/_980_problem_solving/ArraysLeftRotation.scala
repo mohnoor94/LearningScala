fd5149f799f94bf430fa4a84bf5bb0c2c0648f1e
abukhleif <moh.noor94@gmail.com>
2018-08-18 15:44:39 +0300

    Update 'ArraysLeftRotation' solution

--- src/main/scala/_980_problem_solving/ArraysLeftRotation.scala
+++ src/main/scala/_980_problem_solving/ArraysLeftRotation.scala
@@ -1,7 +1,5 @@
 package _980_problem_solving
 
-import java.io.PrintWriter
-
 /**
   * Arrays: Left Rotation
   * For problem description:
@@ -9,23 +7,14 @@ import java.io.PrintWriter
   * @see https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
   */
 object ArraysLeftRotation {
-  def rotLeft(a: Array[Int], d: Int): Array[Int] = {
+  def rotateLeft(a: Array[Int], d: Int): Array[Int] = {
     val aLength = a.length
     val b = new Array[Int](aLength)
     for (i <- a.indices) b((aLength - d + i).abs % aLength) = a(i)
     b
   }
 
-  def main(args: Array[String]) {
-    val stdin = scala.io.StdIn
-//    val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
-    val nd = stdin.readLine.split(" ")
-    val n = nd(0).trim.toInt
-    val d = nd(1).trim.toInt
-    val a = stdin.readLine.split(" ").map(_.trim.toInt)
-    val result = rotLeft(a, d)
-//    printWriter.println(result.mkString(" "))
-    println(result.mkString(" "))
-//    printWriter.close()
+  def main(args: Array[String]): Unit = {
+    println(rotateLeft(Array(1, 2, 3, 4, 5), 4).mkString(", "))
   }
 }
